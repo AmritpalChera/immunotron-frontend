e0594b3ac93aa412045f5e222bee7f60fcfc23f2
Amritpal Chera <seeker@v1040-wn-rt-a-37-201.campus-dynamic.uwaterloo.ca>
2024-03-10 21:03:18 -0400

    inital commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,82 @@
+"use client";
 import Image from "next/image";
+import backend from "./axiox";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [message, setMessage] = useState('Ready!');
+
+  const sendInjectCommand = async () => {
+    const data = await backend.post('/injection', {}).then(res => res.data).catch(err => err.response.data);
+    console.log('Returned: ', data);
+  }
+
+  const getDisplayMessage = (encodedString: string) => {
+    let message = "";
+    if (encodedString === "idle") {
+      message = "Ready!";
+    }
+    else if (encodedString === "determine_injection_location") {
+      message = "Analyzing";
+    }
+    else if (encodedString === "vaccine_delivery") {
+      message = "Injecting Vaccine";
+    }
+    else if (encodedString === "vaccine_disposal") {
+      message = "Disposing Vaccine";
+    }
+    else if (encodedString === "error") {
+      message = "Error";
+    }
+    else if (encodedString === "vaccine_retreival") {
+      message = "Retreiving Vaccine";
+    }
+    message && setMessage(message);
+  }
+
+  const getUpdatedData = async () => {
+    const data = await backend.post('/getStatus').then(res => res.data).catch(err => console.log(err.response.data));
+    if (data?.state) getDisplayMessage(data.state);
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(getUpdatedData, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const engagePlunge = () => backend.post('/engage_plunge');
+  const retractPlunge = () => backend.post('/retract_plunge');
+  const engageDisposal = () => backend.post('/engage_disposal');
+  const retractDisposal = () => backend.post('/retract_disposal');
+  
+  const clearError = () => backend.post('/clear_error');
+  
+
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="text-center">
+        <b className="text-3xl">Immunotron</b>
+        <p className="mt-8 text-green-600 font-bold">{message}</p>
+      </div>
+      
+      <button onClick={sendInjectCommand} className="bg-green-500 px-8 w-64 py-4 rounded">Inject</button>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <button onClick={engagePlunge} className="bg-purple-300 md:w-48 px-8 py-4 rounded">Engage Plunge</button>
+          <button onClick={retractPlunge} className="bg-purple-300 md:w-48 px-8 py-4 rounded">Retract Plunge</button>
         </div>
+        
+        <div className="flex gap-4">
+          <button onClick={engageDisposal} className="bg-purple-300 md:w-48 px-8 py-4 rounded">Engage Dispose</button>
+          <button onClick={retractDisposal} className="bg-purple-300 md:w-48 px-8 py-4 rounded">Retract Dispose</button>
+        </div>
+       
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button onClick={clearError} className="bg-red-500 px-8 w-64 py-4 rounded">Clear Error</button>
     </main>
   );
 }
